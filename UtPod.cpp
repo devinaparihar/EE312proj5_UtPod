80dddfc757fa11fde067025160dacdeb9b7bf79b
esarmah100 <esarmah100@gmail.com>
2018-10-23 16:17:54 -0500

    functions
    Shuffle work in progress

--- UtPod.cpp
+++ UtPod.cpp
@@ -1,3 +1,4 @@
+#include <ctime>
 #include "UtPod.h"
 
 
@@ -29,7 +30,26 @@ int UtPod ::removeSong(Song const &s) {
 }
 
 void UtPod ::shuffle() {
+    int total = 0;
+    SongNode *p;
+    songs = p;
+    while (p != NULL){
+        total++;
+        p->next;
+    }
 
+    unsigned int currentTime = (unsigned)time(0);
+    srand(currentTime);        //seeds the random number generator
+
+    for (int i = 0; i < total; i++){
+        SongNode *temp1 = songs;
+        SongNode *temp2 = songs;
+
+        
+
+        }
+
+    }
 
 }
 
--- song.cpp
+++ song.cpp
@@ -58,6 +58,6 @@ bool Song ::operator>(Song const &rhs) {
 bool Song ::operator==(Song const &rhs) {
     return(artist == rhs.artist && title == rhs.title && size == rhs.size);
 
-// hi eeshan
+// hi whats up
 
 }
